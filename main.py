a12a0aec60babceac9c99a272b2d6682daaaf872
Sasha <dsadsadasda>
2024-08-05 18:07:27 +0300

    1 YROK

---
+++ main.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+# # print('юстус') 
+# # print("мой возраст 11 лет") 
+# # print('Hello world') 
+# print((2+2/4)*4)
+# print((493*4)/6 + 48) 
+# print(((3+2)*4)/((2**2)+3/4))
+# print(((3-(-3**2))/(9*3))*4) 
+a = "мой возраст" 
+b = 11 
+c = 'лет.'
+print(a + str(b) + c)
+a = '10' 
+b = 2.0
+print(a + b)
+a = '28.5' 
+b 
